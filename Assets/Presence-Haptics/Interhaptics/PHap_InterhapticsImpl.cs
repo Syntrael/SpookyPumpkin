6a1c9844aaf2b2c1ba7a54b420a247c0d437bb5f
VR laptop Hackathon <marshall.syntrael@gmx.de>
2025-10-18 14:55:12 +0200

    added file

--- Assets/Presence-Haptics/Interhaptics/PHap_InterhapticsImpl.cs
+++ Assets/Presence-Haptics/Interhaptics/PHap_InterhapticsImpl.cs
@@ -19,7 +19,6 @@ using UnityEngine;
 using Interhaptics.Core;
 using Interhaptics.HapticBodyMapping; // Added for BodyPartE
 using Interhaptics;
-using Interhaptics.Platforms.Sensa;
 #endif
 
 namespace Presence
@@ -120,11 +119,13 @@ namespace Presence
                 return true;
             // For Freyja, we might need a more specific check if available from Interhaptics or RazerSensaProvider
 #if IH_PACKAGE_PRESENT
+#if (UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN)
             var provider = new Interhaptics.Platforms.Sensa.RazerSensaProvider();
             if (provider.IsPresent()) { 
                 //Debug.Log("Interhaptics Device is present.");
                 return true;
             }
+#endif
 #endif
             return false;
         }
